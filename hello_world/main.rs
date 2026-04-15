6a968199c925e4135eebe063013ad25f00832d65
Lucas G. Bueno <lucasgbueno@proton.me>
2026-04-14 23:52:31 -0300

    testing types and parses

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut number = String::from("coisa");
+
+    println!("{number}");
+    number.clear();
+
+    io::stdin().read_line(&mut number).expect("Erro");
+
+    let number: u32 = match number.trim().parse() {
+        Ok(num) => num,
+        Err(_err) => 0,
+    };
+
+    println!("Numero parseado {}", number);
 }
